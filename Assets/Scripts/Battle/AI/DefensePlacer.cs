dc5ce9df36e3fdfe6369fbee83a1bb504be4047f
Juan Rodriguez <juan.rodriguez@etermax.com.uy>
2019-04-04 14:57:53 -0300

    added force field and some more work on defense placer

--- Assets/Scripts/Battle/AI/DefensePlacer.cs
+++ Assets/Scripts/Battle/AI/DefensePlacer.cs
@@ -15,6 +15,44 @@ namespace Battle.AI
         public Transform TurretContainer;
 
         void AddRefinery() => CreateBuilding(Prefabs.Refinery, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        
+        void AddShieldGenerator() => CreateBuilding(Prefabs.Refinery, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        
+        void AddShipyard() => CreateBuilding(Prefabs.Refinery, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        
+        void AddMassDriver() => CreateBuilding(Prefabs.Refinery, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        
+        void AddBunker() => CreateBuilding(Prefabs.Refinery, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        
+        void AddCruisers(int count)
+        {
+            
+        }
+        
+        void AddTanks(int count)
+        {
+            
+        }
+        
+        void AddDrones(int count)
+        {
+            CreateBuilding(Prefabs.DroneTower, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        }
+        
+        void AddInterceptors(int count)
+        {
+            CreateBuilding(Prefabs.DroneTower, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        }
+        
+        void AddTurrets(int count)
+        {
+            CreateBuilding(Prefabs.TurretControl, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        }
+        
+        void AddSAMs(int count)
+        {
+            CreateBuilding(Prefabs.SAMTower, BuildingContainer, MainBuildingSpots.ConsumeSpot());
+        }
 
         GameObject CreateBuilding(GameObject prefab, Transform container, Transform position)
         {
