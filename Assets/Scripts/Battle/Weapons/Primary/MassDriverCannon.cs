b9098d000dfb8a22c1012fea704333adaae44354
Juancho <juan.rod707@gmail.com>
2019-05-17 00:13:56 -0300

    Added mass driver shot

--- Assets/Scripts/Battle/Weapons/Primary/MassDriverCannon.cs
+++ Assets/Scripts/Battle/Weapons/Primary/MassDriverCannon.cs
@@ -1,17 +1,18 @@
-﻿using Battle.Effects;
-using Battle.Entities;
+﻿using Battle.Weapons.Secondary;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Battle.Weapons.Primary
 {
     public class MassDriverCannon : Weapon
     {
+        public Rocket ProjectilePrefab;
         public ParticleSystem Muzzle;
         public Animator Animator;
 
         protected override void FireRound(Vector3 _, Transform __)
         {
+            var shot = Instantiate(ProjectilePrefab, Muzzle.transform.position, transform.rotation);
+            shot.Launch(DamageMessage.Create(Stats.Damage, Stats.DamageTypes));
             Animator.SetTrigger("Fire");
             Muzzle.Play();
             FireSfx.Play();
