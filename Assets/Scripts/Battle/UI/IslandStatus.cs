4dda20139ceea24ddf8fa3d9e0c14e3eee0554d9
Juancho <juan.rod707@gmail.com>
2019-05-27 08:49:22 -0300

    building status wip

---
+++ Assets/Scripts/Battle/UI/IslandStatus.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Battle.UI
+{
+    public class IslandStatus : MonoBehaviour
+    {
+        public BuildingItem BuildingItemPrefab;
+
+        public void AddBuilding()
+        {
+            Instantiate(BuildingItemPrefab, transform);
+        }
+    }
+}
